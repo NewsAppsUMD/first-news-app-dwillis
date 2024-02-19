44fe24050dae86c24ed6b2d0510d293794712427
Derek Willis <dwillis@gmail.com>
2024-02-19 21:47:42 +0000

    upgrades

--- freeze.py
+++ freeze.py
@@ -1,11 +1,6 @@
 from flask_frozen import Freezer
-from app import app, get_csv
+from app import app
 freezer = Freezer(app)
-
-@freezer.register_generator
-def detail():
-    for row in get_csv():
-        yield {'call_number': row['callNumber']}
 
 if __name__ == '__main__':
     freezer.freeze()
